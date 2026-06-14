d4f4bd0c79b0ea5646e998f179151624f22a9e9e
avriella-j <ajimenez2@students.rj.nsw.edu.au>
2026-06-14 16:34:13 +1000

    flask app factory, flask login, socketIO

--- app/__init__.py
+++ app/__init__.py
@@ -20,7 +20,7 @@ def create_app(config_name='default'):
     
     @login_manager.user_loader
     def load_user(user_id):
-        return User.query.get(int(user_id))
+        return db.session.get(User, int(user_id))
     
     # Register blueprints
     from app.auth.routes import auth_bp
