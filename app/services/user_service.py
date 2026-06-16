9544ea4630193d57b5b156d9f61068fda381801c
avriella-j <ajimenez2@students.rj.nsw.edu.au>
2026-06-16 21:12:32 +1000

    fixed user service bug

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timezone
 from app.models.user import User
 from app.extensions import db
 from app.services.token_service import TokenService
@@ -25,6 +26,7 @@ class UserService:
     def get_or_create_user(spotify_id, display_name, email, images, access_token, refresh_token, expires_at):
         """Get existing user or create new one."""
         user = UserService.get_user_by_spotify_id(spotify_id)
+        expires_dt = datetime.fromtimestamp(expires_at, tz=timezone.utc)
         
         if not user:
             image_url = images[0]['url'] if images else None
@@ -35,7 +37,7 @@ class UserService:
                 image_url=image_url,
                 access_token=TokenService.encrypt_token(access_token),
                 refresh_token=TokenService.encrypt_token(refresh_token),
-                token_expires_at=expires_at
+                token_expires_at=expires_dt
             )
             db.session.add(user)
         else:
@@ -43,7 +45,7 @@ class UserService:
             user.email = email
             user.access_token = TokenService.encrypt_token(access_token)
             user.refresh_token = TokenService.encrypt_token(refresh_token)
-            user.token_expires_at = expires_at
+            user.token_expires_at = expires_dt
         
         db.session.commit()
         return user
